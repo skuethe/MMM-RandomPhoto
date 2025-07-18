ff87f77365ea559cb5b830dcb0f71beca4f4d36b
Henry Nguyen <43089637+henrynvn09@users.noreply.github.com>
2025-07-18 12:57:43 -0700

    reindent

--- MMM-RandomPhoto.js
+++ MMM-RandomPhoto.js
@@ -26,7 +26,7 @@ Module.register("MMM-RandomPhoto",{
             username: "",
             password: "",
             recursive: false,
-			exclude: [],
+            exclude: [],
         },
         width: 1920,
         height: 1080,
